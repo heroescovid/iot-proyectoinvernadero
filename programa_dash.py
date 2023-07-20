de05056dce6e2a16455330f5fa2ac5ef359b031f
heroescovid <66176503+heroescovid@users.noreply.github.com>
2023-07-20 15:45:57 -0600

    samuel

--- programa_dash.py
+++ programa_dash.py
@@ -1,7 +1,13 @@
-import dash
-from    dash import html
-from    dash import dcc
-app = dash.Dash(__name__)
-print("Holis a todos !!")
-if __name__ == "__main__":
-    app.run_server("0.0.0.0",5050,debug = True)
+from flask import Flask,render_template,url_for,request,jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+app.route("/")
+def index():
+    return render_template("index.html")
+app.route("/registro")
+def reigstro():
+    return render_template(reigstro.html)
+app.route("/tablas")
+def tablas():
+    return render_template("tablas.html")
+app.run("0.0.0.0",port = 8897,debug = False)
